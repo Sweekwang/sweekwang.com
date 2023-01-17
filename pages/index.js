302190fd47fdbb8d625090e2c8825ec52d42387e
sweekwang <sweekwang94@gmail.com>
2023-01-17 19:25:21 +0800

    Add carousell icons

--- pages/index.js
+++ pages/index.js
@@ -31,18 +31,124 @@ export default function Home() {
         <div className={Style.heroSection}>
           <div className={Style.heroLeft}>
             <div className={Style.heroLeftContent}>
-              <p>Hello I&apos;m</p>
+              <p className={Style.heroCaption}>Hello I&apos;m</p>
               <h1>SWEE KWANG</h1>
-              <p>Junior iOS developer with UIKit and SwiftUI experiences. I love building mobile and web application which can make life simplier and easier.</p>
+              <p className={Style.heroDescription}>Junior iOS developer with UIKit and SwiftUI experiences who write clean and elegant code. I love building mobile and web application.</p>
             </div>
           </div>
 
           <div className={Style.heroRight}>
-          <Image 
-            alt="Chua Swee Kwang"
-            src="/home/sweekwang.png" 
-            width={420} 
-            height={550}/>
+            <Image 
+              alt="Chua Swee Kwang"
+              src="/home/sweekwang.png" 
+              className={Style.myImage}
+              width={420} 
+              height={550}/>
+
+              <div className={Style.logoDiv}>
+                <div className={Style.logoGroup1}>
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swiftui.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swift.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/reactjs.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/nextjs.png" 
+                    width={100} 
+                    height={100}/>
+                </div>
+
+                <div className={Style.logoGroup2}>
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swiftui.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swift.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/reactjs.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/nextjs.png" 
+                    width={100} 
+                    height={100}/> 
+                </div>
+
+                <div className={Style.logoGroup3}>
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swiftui.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swift.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/reactjs.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/nextjs.png" 
+                    width={100} 
+                    height={100}/> 
+                </div>
+                <div className={Style.logoGroup4}>
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swiftui.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/swift.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/reactjs.png" 
+                    width={100} 
+                    height={100}/>
+
+                  <Image 
+                    alt="SwiftUI Developer"
+                    src="/logo/nextjs.png" 
+                    width={100} 
+                    height={100}/> 
+                </div>
+              </div>
           </div>
         </div>
       </main>
